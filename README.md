a1c3e261d7bcb381be1f8a70a2217c833f099fff
Rin <133926402+NazumiRin@users.noreply.github.com>
2024-03-06 19:48:03 -0300

    cargo init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Iris
+
+Iris project
+
+W.I.P.
---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+/// # W.I.P.
+/// Intended to be replaced
+pub fn wip() {
+    println!("W.I.P");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        wip();
+    }
+}
